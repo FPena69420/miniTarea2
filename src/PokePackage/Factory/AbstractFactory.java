1fec6038fbdab13348d99bd34a9bc6831afaf89a
FPena69420 <fpenamarchant@gmail.com>
2021-06-04 16:50:59 -0400

    Versión 3.1

--- src/PokePackage/Factory/AbstractFactory.java
+++ src/PokePackage/Factory/AbstractFactory.java
@@ -1,2 +1,67 @@
-package PokePackage.Factory;public class AbstractFactory {
+package PokePackage.Factory;
+
+public abstract class AbstractFactory implements PokeFactory{
+    private String name= "";
+    private String species= "";
+    private int maxHP= -1;
+    private int maxPP= -1;
+
+
+    @Override
+    /** Define el atributo name */
+    public void setName(String aName) {
+        name= aName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    /** Define el atributo species */
+    public void setSpecies(String aSpecies) {
+        species= aSpecies;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    @Override
+    /** Define el atributo maxHP */
+    public void setMaxHP(int aMaxHP) {
+        maxHP= aMaxHP;
+    }
+
+    public int getMaxHP() {
+        return maxHP;
+    }
+
+    @Override
+    /** Define el atributo maxPP */
+    public void setMaxPP(int aMaxPP) {
+        maxPP= aMaxPP;
+    }
+
+    public int getMaxPP() {
+        return maxPP;
+    }
+
+    @Override
+    /** Establece los atributos a sus valores de inicio */
+    public void reset() {
+        name= "";
+        species= "";
+        maxHP= -1;
+        maxPP= -1;
+    }
+
+    @Override
+    /** Establece todos los atributos al mismo tiempo */
+    public void setFull(String aName, String aSpecies, int aMaxHP, int aMaxPP) {
+        name= aName;
+        species= aSpecies;
+        maxHP= aMaxHP;
+        maxPP= aMaxPP;
+    }
 }
--- src/PokePackage/Factory/WaterFactory.java
+++ src/PokePackage/Factory/WaterFactory.java
@@ -2,54 +2,20 @@ package PokePackage.Factory;
 
 import PokePackage.Pokemon.*;
 
-public class WaterFactory implements PokeFactory{
+public class WaterFactory extends AbstractFactory{
 
-    private String name= "";
-    private String species= "";
-    private int maxHP= -1;
-    private int maxPP= -1;
-
+    public WaterFactory() {
+        super();
+    }
 
     public WaterType create() {
-        return new WaterType(name, species, maxHP, maxPP);
-    }
-
-    @Override
-    /** Define el atributo name */
-    public void setName(String aName) {
-        this.name= aName;
-    }
-
-    @Override
-    /** Define el atributo species */
-    public void setSpecies(String aSpecies) {
-        this.species= aSpecies;
-    }
-
-    @Override
-    public void setMaxHP(int aMaxHP) {
-        this.maxHP= aMaxHP;
-    }
-
-    @Override
-    public void setMaxPP(int aMaxPP) {
-        this.maxPP= aMaxPP;
-    }
-
-    @Override
-    public void reset() {
-        name= "";
-        species= "";
-        maxHP= -1;
-        maxPP= -1;
-    }
-
-    @Override
-    public void setFull(String aName, String aSpecies, int aMaxHP, int aMaxPP) {
-        name= aName;
-        species= aSpecies;
-        maxHP= aMaxHP;
-        maxPP= aMaxPP;
+        try {
+            return new WaterType(this.getName(), this.getSpecies(), this.getMaxHP(), this.getMaxPP());
+        }
+        catch (Exception e){
+            System.out.println("There was an error trying to create the Pokemon");
+            return null;
+        }
     }
 
 }
